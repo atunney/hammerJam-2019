6334703be845158bc01c6403c0843ddbb1aa2601
dog-eared <evan.mulrooney@mohawkcollege.ca>
2019-04-27 09:31:41 -0400

    First actual commit

---
+++ Assets/PipeBoxBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeBoxBehaviour : MonoBehaviour {
+
+	public static List<PipeBoxBehaviour> pipeBoxes;
+	public int selectedPipeIndex = 0;
+
+	void Awake() {
+		if (!pipeBoxes.Contains(this)) {
+			pipeBoxes.Add(this);
+		}
+	}
+
+
+	void Update() {
+		if (Input.GetKeyDown("s") && pipeBoxes[selectedPipeIndex] == this) {
+			Debug.Log("Pipe box: " + selectedPipeIndex + " drop");
+		}
+
+
+	}
+
+
+}
