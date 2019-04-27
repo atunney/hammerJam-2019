fe7682b027c8b409057b0acc09c56aada99d2e31
dog-eared <evan.mulrooney@mohawkcollege.ca>
2019-04-27 19:29:20 -0400

    presentation ready

---
+++ Assets/ToiletSfx.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletSfx : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
